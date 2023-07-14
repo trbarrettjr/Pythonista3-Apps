0e9d8f82ce5a221d763124dc99f824255a2b0305
Thomas Barrett <trbarrettjr@users.noreply.github.com>
2023-07-14 12:05:07 -0700

    Update aurora-map.py

--- aurora-map.py
+++ aurora-map.py
@@ -6,6 +6,13 @@ import json
 import datetime
 from PIL import Image
 
+'''This program checks the Space Weather Prediction Center for the current
+Status of the Aurora.
+
+Please adjust certain parameters'''
+
+my_email = '' # Place your email here to allow them to contact you in the event of an issue.
+
 def getLocation():
 	if location.is_authorized():
 		location.start_updates()
@@ -13,21 +20,29 @@ def getLocation():
 		location.stop_updates()
 	else:
 		print("Location is not authorized.  Please open settings to authorize location")
-		exit(1)
+		exit() # Exit codes > 0 don't work properly in Pythonista
 		
 	return myLocation
 		
-def getMap():
-	conn = ("services.swpc.noaa.gov", 
-	"/images/aurora-forecast-northern-hemisphere.jpg")
+def getMap(latitude):
+	if latitude > 0:
+		conn = ("services.swpc.noaa.gov", "/images/aurora-forecast-northern-hemisphere.jpg")
+	else:
+		conn = ("services.swpc.noaa.gov", "/images/aurora-forecast-southern-hemisphere.jpg")
 	h1 = http.client.HTTPSConnection(conn[0])
-	h1.request("GET", conn[1], None, headers={"User-Agent": "AuroraPredict/1.1a email@domain.tld"}) # Provide an email address in the event they need to contact you.
-	resp = h1.getresponse()
-	if resp.status in range(200, 300):
+	h1.request(
+			"GET", 
+			conn[1], 
+			None, 
+			headers={"User-Agent": f"AuroraPredict/1.1a {my_email}"}
+		)
+	
+	resp = h1.getresponse() # Get the HTTP Response
+	if resp.status in range(200, 300): # Check to make sure the response is good.
 		image = resp.read()
 	else:
 		print("Something isn't right.", resp.status, resp.reason)
-		exit(2)
+		exit() # Exit if something is wrong.
 		
 	image = Image.open(io.BytesIO(image))
 	h1.close()
@@ -41,13 +56,14 @@ def getIndex(lat, long):
 	conn[1], 
 	None, 
 	headers={
-		"User-Agent": "AuroraPredict/1.1a email@domain.tld", # provide an email address in the event they need to contact you.
+		"User-Agent": f"AuroraPredict/1.1a {my_email}", # Add email in the event of needing to contact
 		"Accept": "application/json"})
 	resp = h2.getresponse()
 	if resp.status in range(200, 300):
 		data = json.load(resp)
 	else:
 		print("Something is wrong!", resp.status, resp.reason)
+		exit(2) # Exit > 0 is an error, I am using exit 2 as HTTP Error
 		
 	lat = int(lat)
 	if long < 0:
@@ -64,22 +80,14 @@ def getIndex(lat, long):
 def main():
 	my_location = getLocation()
 	my_index = getIndex(my_location["latitude"], my_location["longitude"])
-	image_map = getMap()
-	print("Lat:  {}\n" \
-       "Long: {}\n" \
-		"Indx: {}\n\n" \
-		"UTC\ntime:\t\t{}\n" \
-		"Observation:\t{}\n" \
-		"Forcast:\t{}".format(
-			my_location["latitude"], 
-			my_location["longitude"], 
-			my_index[0][2], 
-			datetime.datetime.utcnow(),
-			my_index[1],
-			my_index[2]
-		)
-	)
-	image_map.show()
+	image_map = getMap(my_location["latitude"]) # Pass the latitude to get the Norther or Southern Hemisphere
+	print(f"Lat:  {my_location['latitude']}\n" \
+       	f"Long: {my_location['longitude']}\n" \
+		f"Indx: {my_index[0][2]}\n\n" \
+		f"UTC\ntime:\t\t{datetime.datetime.utcnow()}\n" \
+		f"Observation:\t{my_index[1]}\n" \
+		f"Forcast:\t{my_index[2]}")
+	image_map.show() # Benefit of Pythonista and PIL is that it will show the image in the console.
 			
 if __name__ == "__main__":
 	main()
